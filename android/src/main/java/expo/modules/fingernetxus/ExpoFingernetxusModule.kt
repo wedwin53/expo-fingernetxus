c4599b450ecd2a72ce86bd94e3bd9499f3276ecc
Edwin Rebolledo <wedwin53@gmail.com>
2023-05-29 23:03:45 -0600

    first commit

--- android/src/main/java/expo/modules/fingernetxus/ExpoFingernetxusModule.kt
+++ android/src/main/java/expo/modules/fingernetxus/ExpoFingernetxusModule.kt
@@ -1,9 +1,27 @@
 package expo.modules.fingernetxus
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import org.json.JSONObject
 
 class ExpoFingernetxusModule : Module() {
+
+  // create a variable to hold if the permission has been granted or not
+
+
+  var permissionGranted: Boolean = false
+  var currentAdapter: BluetoothAdapter? = null
+  var newDevicesList: ArrayList<String> = ArrayList()
+  var pairedDevicesList: ArrayList<String> = ArrayList()
+
+
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
@@ -13,35 +31,76 @@ class ExpoFingernetxusModule : Module() {
     // The module will be accessible from `requireNativeModule('ExpoFingernetxus')` in JavaScript.
     Name("ExpoFingernetxus")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+    // Function to request bluetooth permissions
+    Function("requestBluetoothPermissionsAsync") {
+      val activity = appContext.activityProvider?.currentActivity
+        val bluetoothPermissions = arrayOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
+      val applicationContext = activity?.applicationContext
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
+      if(applicationContext != null) {
+        Log.i("ExpoFingernetxusModule", "Requesting bluetooth permissions")
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ExpoFingernetxusView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ExpoFingernetxusView, prop: String ->
-        println(prop)
+        val permissionCheck = ContextCompat.checkSelfPermission(
+          applicationContext,
+          Manifest.permission.BLUETOOTH
+        )
+        Log.i("ExpoFingernetxusModule", "Permission check: $permissionCheck")
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+          ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(bluetoothPermissions[0], bluetoothPermissions[1], bluetoothPermissions[2], bluetoothPermissions[3]),
+            1
+          )
+        } else {
+          Log.i("ExpoFingernetxusModule", "Bluetooth permissions already granted")
+          permissionGranted = true
+
+          // BLUETOOTH CONNECTION
+          currentAdapter = BluetoothAdapter.getDefaultAdapter()
+          if (currentAdapter == null) {
+            Log.i("ExpoFingernetxusModule", "No bluetooth adapter found")
+          } else {
+            // If we're already discovering, stop it
+            if (currentAdapter!!.isDiscovering) {
+              Log.i("ExpoFingernetxusModule", "Bluetooth adapter is already discovering, stopping discovery")
+              currentAdapter!!.cancelDiscovery()
+            }
+            // Request discover from BluetoothAdapter
+            Log.i("ExpoFingernetxusModule", "Starting discovery")
+            currentAdapter!!.startDiscovery()
+
+            // Get a set of currently paired devices
+            val pairedDevices = currentAdapter!!.bondedDevices
+            Log.i("ExpoFingernetxusModule", "Paired devices: $pairedDevices")
+            // If there are paired devices, add each one to the ArrayAdapter
+            if (pairedDevices.size > 0) {
+              for (device in pairedDevices) {
+                pairedDevicesList.add(device.name + "," + device.address)
+
+              }
+            } else {
+              pairedDevicesList.add("No paired devices found")
+            }
+
+
+          }
+        }
       }
+      return@Function pairedDevicesList
     }
+
+    // Function to Connect to a bluetooth device recieved from the JS side by parameter
+
+
   }
+
+
+
 }
--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { useState } from 'react';
 
 import * as ExpoFingernetxus from 'expo-fingernetxus';
 
 export default function App() {
+  const [result, setResult] = useState<boolean | false>();
+  const [devicesList, setDevicesList] = useState<[]>();
+  
+  const requestBluetoothPermissionRN = () => {
+   const devicesResult = ExpoFingernetxus.requestBluetoothPermission();
+   console.log(devicesList);
+   setDevicesList(devicesResult);
+    setResult(true);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>{ExpoFingernetxus.hello()}</Text>
+      <Text>Permission Status: {result}</Text>
+      <TouchableOpacity onPress={requestBluetoothPermissionRN} 
+      style={styles.button}
+      >
+        <Text style={styles.buttonText} >Request Permission</Text>
+      </TouchableOpacity>
+      <Text>Devices Found:</Text>
+      
+      <FlatList
+        data={devicesList}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
+
+
+
     </View>
   );
 }
@@ -17,4 +43,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  }
 });
